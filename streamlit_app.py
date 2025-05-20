02a2de088849b72a352b77febf43d0f9e575b106
Jojo1424 <97798029+Jojo1424@users.noreply.github.com>
2025-05-20 03:23:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 
-# Load your trained model and vectorizer (must be in the same folder)
 with open("vectorizer.pkl", "rb") as f:
     vectorizer = pickle.load(f)
 with open("rf_model.pkl", "rb") as f:
@@ -13,7 +12,6 @@ user_input = st.text_area("Paste a news article (title + content):")
 
 if st.button("Predict"):
     if user_input.strip():
-        # Apply the same text cleaning you used in training!
         input_clean = user_input.lower()
         input_vec = vectorizer.transform([input_clean])
         pred = model.predict(input_vec)[0]
